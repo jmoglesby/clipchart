c0d7ee7c539235a65220ea23928917af59f9cd42
Jeremy Oglesby <iaminaband@gmail.com>
2017-02-17 12:46:35 -0700

    cleanup of children controller/view - db:migrations need cleanup next

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -4,33 +4,19 @@ class ChildrenController < ApplicationController
   end
 
   def show
-    @child = child.find(params[:id])
   end
 
   def new
-    @child = child.new
   end
 
   def edit
-    @child = child.find(params[:id])
   end
 
   def create
-    @child = child.new(child_params)
-    if @child.save
-      redirect_to @child
-    else
-      render 'new'
-    end
+    render plain: params[:child].inspect
   end
 
   def update
-    @child = child.find(params[:id])
-    if @child.update(child_params)
-      redirect_to @child
-    else
-      render 'edit'
-    end
   end
 
   private
