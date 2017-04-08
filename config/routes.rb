256036960abf44cab4ee2cef4a1115fa5ad179e5
Jeremy Oglesby <iaminaband@gmail.com>
2017-04-07 23:49:34 -0700

    Prep for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
 
   resource :money_pool
 
-  root 'children#index'
+  root 'drinks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
